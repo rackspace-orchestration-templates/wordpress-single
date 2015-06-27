74ec862a6ac3d5908178cb8b2ce42348ff16647f
Brint O'Hearn <brint.ohearn@rackspace.com>
2015-06-27 12:01:32 -0500

    Removing cookbooks

--- cookbooks/apache2/spec/support/platform_properties.rb
+++
@@ -1,5 +0,0 @@
-require 'json'
-
-def load_platform_properties(args)
-  JSON.parse(IO.read(File.join(File.dirname(__FILE__), "/../../test/fixtures/platforms/#{args[:platform]}/#{args[:platform_version]}.json")), :symbolize_names => true)
-end
